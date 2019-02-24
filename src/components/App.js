508c4d7aa320e78ee81c658289e38fb044c87ea4
mehmteke <teker.mehmed@gmail.com>
2019-02-24 20:48:22 +0300

    BrowserRouter,Provider kurulumlari

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+//import logo from '../logo.svg';
+import logo from '../images/intertech.jpg';
+import '../Styles/App.css';
+import {Link,Route} from 'react-router-dom';
+import AccountsPage from '../components/pages/AccountsPage'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+         
+        </header>
+        <p className="App-intro">
+            <Link to="accounts">Accounts</Link>
+          </p>
+          <Route path='./accounts' component={AccountsPage}></Route>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './Styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import {createStore,applyMiddleware} from 'redux';
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
-
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 
 const store = createStore(
   rootReducer,
@@ -18,7 +19,11 @@ const store = createStore(
 
 ReactDOM.render 
 ( 
-    <App />, 
+    <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    </BrowserRouter>, 
     document.getElementById('root')
 );
 
